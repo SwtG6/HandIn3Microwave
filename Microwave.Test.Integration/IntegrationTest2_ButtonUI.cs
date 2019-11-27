10a893ffa12ea6258a5c4344115175d41c28807a
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-27 19:12:56 +0100

    IntegrationTest2_ButtonUI Ready for test implementations

--- Microwave.Test.Integration/IntegrationTest2_ButtonUI.cs
+++ Microwave.Test.Integration/IntegrationTest2_ButtonUI.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
@@ -18,10 +20,57 @@ namespace Microwave.Test.Integration
 
         private IDoor _door;
 
-        private IUserInterface _UUT;
+        private IUserInterface _uut;
         private ILight _light;
         private IDisplay _display;
 
         private ICookController _cookController;
+
+        #region SetUp
+
+        [SetUp]
+        public void SetUp()
+        {
+            _startCancelButton = new Button();
+            _powerButton = new Button();
+            _timeButton = new Button();
+
+            _door = Substitute.For<IDoor>();
+
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+
+            _cookController = Substitute.For<ICookController>();
+
+
+            _uut = new UserInterface
+            (
+                _startCancelButton,
+                _powerButton,
+                _timeButton,
+                _door,
+                _display,
+                _light,
+                _cookController
+            );
+        }
+
+        #endregion SetUp
+
+
+        #region Tests
+
+        [Test]
+        public void StartCancelButton_StartTest()
+        {
+            _startCancelButton.Press();
+
+
+        }
+
+
+
+        #endregion Tests
+
     }
 }
