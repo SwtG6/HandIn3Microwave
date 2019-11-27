c1028be8ffaf3dd20c7c2dfd191a3dc302636ae5
Bl0bb3r <34944047+Bl0bb3r@users.noreply.github.com>
2019-11-27 18:38:17 +0100

    added missing nuget (for our own project - Microwave.Test.Integration) for all to use - NUnit

--- Microwave.Test.Integration/IntegrationTest1_DoorUI.cs
+++ Microwave.Test.Integration/IntegrationTest1_DoorUI.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 
 namespace Microwave.Test.Integration
@@ -15,8 +19,42 @@ namespace Microwave.Test.Integration
         private IButton _timeButton;
         private IButton _startCancelButton;
 
+        private ICookController _cookController;
+        private IDisplay _display;
+        private IDoor _door;
+        private ILight _light;
+        private IUserInterface _uut;
 
-        
+        [SetUp]
+        public void SetUp()
+        {
+            //fakes
+            _cookController = Substitute.For<ICookController>();
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+
+
+            //Ctors
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+
+            //uut
+            _uut = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+
+
+        }
+
+        [Test]
+        public void OpenDoor()
+        {
+
+        }
+
+
+
 
     }
+
 }
