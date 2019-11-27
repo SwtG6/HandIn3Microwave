4a27132ac0124f4f50937b70091adfd3c082ff95
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-27 18:43:36 +0100

    Added start of ButtonUI test

---
+++ Microwave.Test.Integration/IntegrationTest2_ButtonUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class IntegrationTest2_ButtonUI
+    {
+        private IButton _startCancelButton;
+        private IButton _powerButton;
+        private IButton _timeButton;
+
+        private IDoor _door;
+
+        private IUserInterface _UUT;
+        private ILight _light;
+        private IDisplay _display;
+
+        private ICookController _cookController;
+    }
+}
