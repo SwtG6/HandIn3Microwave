1b65f3f4673e241c79e7da759e3bac4c6b5df73f
Bl0bb3r <34944047+Bl0bb3r@users.noreply.github.com>
2019-11-27 18:15:18 +0100

    first integration test initiated

--- Microwave.Test.Integration/IntegrationTest1_DoorUI.cs
+++ Microwave.Test.Integration/IntegrationTest1_DoorUI.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Interfaces;
+
 
 namespace Microwave.Test.Integration
 {
     class IntegrationTest1_DoorUI
     {
+        //Referenced interfaces constructed for usage
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+
+
+        
+
     }
 }
