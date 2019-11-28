bfd462b7a3ef3290b5822bf7cabea80d72b2c382
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-28 12:02:58 +0100

    Tilføjede enkelte UC-tests for StartCancelButton og startede på PowerButton

--- Microwave.Test.Integration/IntegrationTest2_ButtonUI.cs
+++ Microwave.Test.Integration/IntegrationTest2_ButtonUI.cs
@@ -57,20 +57,53 @@ namespace Microwave.Test.Integration
 
         #endregion SetUp
 
+        #region Små tests af enkelte Use Case steps
 
-        #region Tests
-
-        [Test]
-        public void StartCancelButton_StartTest()
+        [Test] // Test 1: Der trykkes på StartCancelButton og lyset i mikrobølgeovnen tændes. UC: 8 & 9.
+        public void StartCancelButton_LightTest()
         {
             _startCancelButton.Press();
+
+            _light.Received(1).TurnOn();
+        }
+
+        [Test] // Test 2: Der trykkes på StartCancelButton under Power-setup og displayet blankes. UC Extension 1.
+        public void StartCancelButton_DisplayTest()
+        {
+            _startCancelButton.Press();
+
+            _display.Received(1).Clear();
+        }
+
+        [Test] // Test 3: Der trykkes op StartCancelButton under cooking og PT slukkes samt displayet blankes. UC Extension 3.
+        public void StartCancelButton_PTDisplayTest()
+        {
+            _startCancelButton.Press();
+
+            _cookController.Received(1).Stop();
+        }
+
+        [Test] // Test 4: Der trykkes på PowerButton én eller flere gange, og det valgte power level ændres samt opdateres på displayet. UC 6.
+        public void PowerButton_SettingDisplayTest()
+        {
+            _powerButton.Press();
+
+        }
+
+        #endregion Små tests af enkelte Use Case steps
+
+        #region Fulde Use case tests
+
+        [Test]
+        public void testetstetsts()
+        {
+
 
 
         }
 
 
-
-        #endregion Tests
+        #endregion Fulde Use case tests
 
     }
 }
