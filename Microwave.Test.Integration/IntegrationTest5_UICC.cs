53af53bc06634b5a0fd64399801f4f50ab17dfce
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-12-10 15:56:30 +0100

    Fuld indkommentering af tests i IT5

--- Microwave.Test.Integration/IntegrationTest5_UICC.cs
+++ Microwave.Test.Integration/IntegrationTest5_UICC.cs
@@ -62,7 +62,7 @@ namespace Microwave.Test.Integration
 
         #region Fulde Use case tests
 
-        [Test]
+        [Test] // Pre-test - Er der forbindelse mellem komponenterne?
         public void UserInterfaceCookController_TurnOnTest()
         {
             _powerButton.Press();
@@ -74,143 +74,143 @@ namespace Microwave.Test.Integration
         }
 
 
-        //[Test] // Test 1: Tester om Power Tube bliver indstillet til det rigtige Power level ved ét tryk på powerknappen. UC 1-10.
-        //public void CorrectPowerOnCookStart_PowerTubeTest()
-        //{
-        //    _door.Open();
-        //    _door.Close();
+        [Test] // Test 1: Tester om Power Tube bliver indstillet til det rigtige Power level ved ét tryk på powerknappen. UC 1-10.
+        public void CorrectPowerOnCookStart_PowerTubeTest()
+        {
+            _door.Open();
+            _door.Close();
 
-        //    _powerButton.Press();
-        //    //_powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _powerButton.Press();
+            //_powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Press();
+            _startCancelButton.Press();
 
-        //    _timer.Received(1).Stop();
-        //    _powerTube.Received(1).TurnOn(50);
-        //}
+            _timer.Received(1).Stop();
+            _powerTube.Received(1).TurnOn(50);
+        }
 
-        //[TestCase(5, 250)]
-        //// Test 2: Tester om Power Tube bliver indstillet til det rigtige Power level ved flere tryk på powerknappen. UC 1-10.
-        //public void CorrectPowerOnCookStart_MultiplePowerTubeTest(int NumberOfPresses, int PowerLevel)
-        //{
-        //    // NumberOfPresses = 5;
-        //    // PowerLevel = 250;
+        [TestCase(5, 250)]
+        // Test 2: Tester om Power Tube bliver indstillet til det rigtige Power level ved flere tryk på powerknappen. UC 1-10.
+        public void CorrectPowerOnCookStart_MultiplePowerTubeTest(int NumberOfPresses, int PowerLevel)
+        {
+            // NumberOfPresses = 5;
+            // PowerLevel = 250;
 
-        //    _door.Open();
-        //    _door.Close();
+            _door.Open();
+            _door.Close();
 
-        //    for(int i = 0; i < NumberOfPresses; i++)
-        //    {
-        //        _powerButton.Press();
-        //    }
+            for (int i = 0; i < NumberOfPresses; i++)
+            {
+                _powerButton.Press();
+            }
 
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
 
-        //    _powerTube.Received(1).TurnOn(PowerLevel);
-        //}
+            _powerTube.Received(1).TurnOn(PowerLevel);
+        }
 
-        //[Test] // Test 3: Tester om Timer bliver indstillet til den rigtige Timer Setting ved ét tryk på timerknappen. UC 1-10.
-        //public void CorrectTimerOnCookStart_TimerTest()
-        //{
-        //    _door.Open();
-        //    _door.Close();
+        [Test] // Test 3: Tester om Timer bliver indstillet til den rigtige Timer Setting ved ét tryk på timerknappen. UC 1-10.
+        public void CorrectTimerOnCookStart_TimerTest()
+        {
+            _door.Open();
+            _door.Close();
 
-        //    _powerButton.Press();
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
 
-        //    // _timer.Received(1).Start(60);   // tid i sekunder, ikke i minutter.
-        //    //_display.Received(1).ShowTime(01, 00);
-        //    //_output.Received(1).OutputLine($"Display shows: {01:D2}:{00:D2}");
-        //    _timer.TimeRemaining.Returns(60);
-        //}
+            // _timer.Received(1).Start(60);   // tid i sekunder, ikke i minutter.
+            //_display.Received(1).ShowTime(01, 00);
+            //_output.Received(1).OutputLine($"Display shows: {01:D2}:{00:D2}");
+            _timer.TimeRemaining.Returns(60);
+        }
 
-        //[TestCase(5, 5)]
-        //// Test 4: Tester om Timer bliver indstillet til den rigtige Timer Setting ved flere tryk på timerknappen. UC 1-10.
-        //public void CorrectTimerOnCookStart_MultipleTimerTest(int NumberOfPresses, int TimerSetting)
-        //{
-        //    // NumberOfPresses = 5;
-        //    // TimerSetting = 5;
+        [TestCase(5, 5)]
+        // Test 4: Tester om Timer bliver indstillet til den rigtige Timer Setting ved flere tryk på timerknappen. UC 1-10.
+        public void CorrectTimerOnCookStart_MultipleTimerTest(int NumberOfPresses, int TimerSetting)
+        {
+            // NumberOfPresses = 5;
+            // TimerSetting = 5;
 
-        //    _door.Open();
-        //    _door.Close();
+            _door.Open();
+            _door.Close();
 
-        //    _powerButton.Press();
+            _powerButton.Press();
 
-        //    for (int i = 0; i < NumberOfPresses; i++)
-        //    {
-        //        _timeButton.Press();
-        //    }
+            for (int i = 0; i < NumberOfPresses; i++)
+            {
+                _timeButton.Press();
+            }
 
-        //    _startCancelButton.Press();
-
-
-        //    _timer.Received(1).Start(TimerSetting*60);   // tid i sekunder, ikke i minutter.
-        //}
+            _startCancelButton.Press();
 
 
-        //[Test] // Test 5: Tester om PowerTube slukker når der trykkes på StartCancelButton under tilberedning. Extension 3.
-        //public void CancelCooking_PowerTubeTest()
-        //{
-        //    //_door.Open();
-        //    //_door.Close();
-
-        //    _powerButton.Press();
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _timer.Received(1).Start(TimerSetting * 60);   // tid i sekunder, ikke i minutter.
+        }
 
 
-        //    _startCancelButton.Press();
-        //    //_powerTube.Received(1).TurnOff();
-        //}
+        [Test] // Test 5: Tester om PowerTube slukker når der trykkes på StartCancelButton under tilberedning. Extension 3.
+        public void CancelCooking_PowerTubeTest()
+        {
+            //_door.Open();
+            //_door.Close();
 
-        //[Test] // Test 6: Tester om Timer slukker når der trykkes på StartCancelButton under tilberedning. Extension 3.
-        //public void CancelCooking_TimerAndPowerTubeTest()
-        //{
-        //    _door.Open();
-        //    _door.Close();
-
-        //    _powerButton.Press();
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
 
 
-        //    _startCancelButton.Press();
-        //    _timer.Received(1).Stop();
-        //}
+            _startCancelButton.Press();
+            //_powerTube.Received(1).TurnOff();
+        }
 
-        //[Test] // Test 7: Tester om PowerTube slukker når døren åbnes under tilberedning. Extension 4.
-        //public void DoorOpenCooking_PowerTubeTest()
-        //{
-        //    _door.Open();
-        //    _door.Close();
+        [Test] // Test 6: Tester om Timer slukker når der trykkes på StartCancelButton under tilberedning. Extension 3.
+        public void CancelCooking_TimerAndPowerTubeTest()
+        {
+            _door.Open();
+            _door.Close();
 
-        //    _powerButton.Press();
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
-
-
-        //    //_door.Opened += Raise.EventWith(this, EventArgs.Empty);
-        //    _door.Open();
-        //    _powerTube.Received(1).TurnOff();
-        //}
-
-        //[Test] // Test 8: Tester om Timer slukker når døren åbnes under tilberedning. Extension 4.
-        //public void DoorOpenCooking_TimerTest()
-        //{
-        //    _door.Open();
-        //    _door.Close();
-
-        //    _powerButton.Press();
-        //    _timeButton.Press();
-        //    _startCancelButton.Press();
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
 
 
-        //    //_door.Opened += Raise.EventWith(this, EventArgs.Empty);
-        //    _door.Open();
-        //    _timer.Received(1).Stop();
-        //}
+            _startCancelButton.Press();
+            _timer.Received(1).Stop();
+        }
+
+        [Test] // Test 7: Tester om PowerTube slukker når døren åbnes under tilberedning. Extension 4.
+        public void DoorOpenCooking_PowerTubeTest()
+        {
+            _door.Open();
+            _door.Close();
+
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+
+            //_door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            _door.Open();
+            _powerTube.Received(1).TurnOff();
+        }
+
+        [Test] // Test 8: Tester om Timer slukker når døren åbnes under tilberedning. Extension 4.
+        public void DoorOpenCooking_TimerTest()
+        {
+            _door.Open();
+            _door.Close();
+
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+
+            //_door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            _door.Open();
+            _timer.Received(1).Stop();
+        }
 
         #endregion Fulde Use case tests
     }
